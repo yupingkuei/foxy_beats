3c28c8812960f85b7f6aff22b342d0f8edd06f03
Liz Vo <liz_quack@hotmail.com>
2020-11-10 22:47:27 +0900

    both sign up and login goes to the last page the user was on

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
 	protected
 
 	def after_sign_up_path_for(resource)
-		vinyls_path
+		# vinyls_path
+		stored_location_for(resource) || super
 	end
 end
 
 def after_sign_in_path_for(resource_or_scope)
-  stored_location_for(resource_or_scope) || super
+	stored_location_for(resource_or_scope) || super
 end
