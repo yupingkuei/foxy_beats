584f35d75138f58df32df7e4e31c6390d26e1ade
Liz Vo <liz_quack@hotmail.com>
2020-11-10 22:40:19 +0900

    redirect to the last page upon login feature added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+	before_action :store_user_location!, if: :storable_location?
+	before_action :authenticate_user!
+	
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -9,5 +11,14 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+	end
+
+	def storable_location?
+		request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+	end
+
+	def store_user_location!
+		# :user is the scope we are authenticating
+		store_location_for(:user, request.fullpath)
 	end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,3 +5,7 @@ class RegistrationsController < Devise::RegistrationsController
 		vinyls_path
 	end
 end
+
+def after_sign_in_path_for(resource_or_scope)
+  stored_location_for(resource_or_scope) || super
+end
