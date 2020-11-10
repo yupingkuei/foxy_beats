73072957cefcc3ec64d84a24cefdddad91058f17
Seigo Jinbo <seigojinbo@gmail.com>
2020-11-10 17:35:31 +0900

    dashboard

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :set_user
+
+  def dashboard
+    @collection = current_user.vinyls
+    @pending_confirmations = current_user.rentals_as_owner.where(status: "pending")
+    @rentals = current_user.rentals
+    @pending_rentals = current_user.rentals.where(status: "pending")
+  end
+
+  def collection
+    @collection = current_user.vinyls
+  end
+
+  def rentals
+    @rentals = current_user.rentals
+  end
+
+  def pending_confirmation
+    @pending_confirmations = current_user.rentals_as_owner.where(status: "pending")
+  end
+
+  def pending_rentals
+    @pending_rentals = current_user.rentals.where(status: "pending")
+  end
+
+  private
+
+  def set_user
+    authorize current_user
+  end
+end
