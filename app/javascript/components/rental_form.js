2f586faea251c02d64f6a1f5f80efd470ba707ce
Yuping Kuei <kueiyuping@gmail.com>
2020-11-13 14:31:45 +0900

    index page styling

--- app/javascript/components/rental_form.js
+++ app/javascript/components/rental_form.js
@@ -1,14 +1,14 @@
-const showRentalForm = () => {
-  const rentalBtn = document.querySelector("#rental-btn");
-  const rentalForm = document.querySelector("#rental-form");
-  if (rentalForm) {
-    rentalBtn.addEventListener("click", (event) => {
-      // Do something (callback)
-      event.preventDefault();
-      rentalForm.style.display = "block";
-      event.currentTarget.style.display = "none";
-    });
-  }
-};
+// const showRentalForm = () => {
+//   const rentalBtn = document.querySelector("#rental-btn");
+//   const rentalForm = document.querySelector("#rental-form");
+//   if (rentalForm) {
+//     rentalBtn.addEventListener("click", (event) => {
+//       // Do something (callback)
+//       event.preventDefault();
+//       rentalForm.style.display = "block";
+//       event.currentTarget.style.display = "none";
+//     });
+//   }
+// };
 
-export { showRentalForm };
+// export { showRentalForm };
